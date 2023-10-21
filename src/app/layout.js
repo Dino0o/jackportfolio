567e5af3172f3295bfd664178733106bf0530b48
Jack DeGesero <degeserojd@gmail.com>
2023-10-21 14:25:21 -0400

    css update 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <Head>      
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <body className={inter.className}>
         <NavBar/>
         {children}
